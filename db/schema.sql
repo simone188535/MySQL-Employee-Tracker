88bd273f7a47357fe632587a3e0c17aba870465b
Simone Anthony <simone.anthony1@yahoo.com>
2021-12-22 09:58:38 -0500

    more additions to sql schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,20 +12,20 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(6,4) NOT NULL,
-  department_id INT,
+  department_id INT DEFAULT NULL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT DEFAULT NULL,
   FOREIGN KEY (role_id)
   REFERENCES role(id)
   ON DELETE SET NULL,
-  manager_id INT,
+  manager_id INT DEFAULT NULL,
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
   ON DELETE SET NULL
